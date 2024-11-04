73ce42d0abaa5cf68262d430dbb17df093463365
Soumili Nayak <155447989+runlovexa@users.noreply.github.com>
2024-11-04 11:19:48 +0530

    Uploading code: Lucky Character.py

---
+++ Lucky Character.py
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LuckyCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        String input = scanner.nextLine();
+        
+        
+        char luckyLetter = input.charAt(6);
+        
+       
+        System.out.println(luckyLetter);
+        
+        scanner.close();
+    }
+}
